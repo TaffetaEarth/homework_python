ecd2f5eea96a8c6fdb97e8ab1bb6b27f2eccde05
TaffetaEarth <yasha10122@gmail.com>
2021-10-24 18:54:57 +0300

    homework update 24th of October

--- 1510/Menu/Menu.py
+++ 1510/Menu/Menu.py
@@ -1,8 +1,26 @@
 import xml.etree.ElementTree as et
+import matplotlib.pyplot as plt
 
 tree = et.ElementTree(file='menu.xml')
 root = tree.getroot()
+x = []
+fig = plt.figure()
+plt.ylabel("Prices")
+plt.xlabel("Dishes")
 for child in root:
-    print('tag:', child.tag, 'attributes:', child.attrib)
-for grandchild in child:
+    for grandchild in child:
+        x.append([child.tag, float(grandchild.attrib['price'][1:]), grandchild.text])
+print(x)
+barlist = plt.bar([value[2] for value in x],
+                  [value[1] for value in x])
+for i in range(len(barlist)):
+    if x[i][0] == "breakfast":
+        barlist[i].set_color('r')
+    if x[i][0] == "lunch":
+        barlist[i].set_color('g')
+    if x[i][0] == "dinner":
+        barlist[i].set_color('b')
+plt.title("Menu")
+plt.grid(True)
+plt.show()
 
